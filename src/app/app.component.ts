73f04cef851dce79574088da9f99444417523684
nedel <nedellec.julien@hotmail.fr>
2024-05-02 21:27:00 +0200

    chore: Remove unused imports in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
